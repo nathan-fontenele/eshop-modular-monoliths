92ff9d05f1eebc2537989836f3e3737e4f9e6422
Nathan Fontenele <nathangf60@outlook.com>
2025-12-05 14:58:14 -0300

    feat\adicionar módulos Basket, Catalog e Ordering com referências de projeto e configuração

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,9 +1,18 @@
+using Basket;
+using Catalog;
+using Ordering;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCatalogModule()
+    .UseBasketModule()
+    .UseOrderiogModule();
 
 app.Run();
