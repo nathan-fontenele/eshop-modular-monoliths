ebbe26a20dd3071eb01ab911a4582dc1992588a7
Nathan Fontenele <nathangf60@outlook.com>
2025-12-26 21:13:47 -0300

    create product module

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,44 @@
+﻿namespace Catalog.Products.Models
+{
+    public class Product : Aggregate<Guid>
+    {
+        public string Name { get; private set; } = default!;
+        public List<string> Category { get; private set; } = new();
+        public string Description { get; private set; } = default!;
+        public string ImageFile { get; private set; } = default!;
+        public decimal Price { get; private set; }
+
+        public static Product Create(string name, List<string> category, string description, string imageFile, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name, nameof(name));
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price, nameof(price));
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Category = category,
+                Description = description,
+                ImageFile = imageFile,
+                Price = price
+            };
+            product.AddDomainEvent(new ProductCreatedEvent(product));
+            return product;
+        }
+
+        public void Update(string name, List<string> category, string description, string imageFile, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name, nameof(name));
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price, nameof(price));
+            Name = name;
+            Category = category;
+            Description = description;
+            ImageFile = imageFile;
+            Price = price;
+
+            if (Price != price)
+            {
+                AddDomainEvent(new ProductPriceChangedEvent(this));
+            }
+        }
+    }
+}
